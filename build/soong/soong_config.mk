491adfea8f6473909f4579a96ff1a1f54d691274
Simon Shields <simon@lineageos.org>
2017-09-04 21:11:16 +1000

    build: soong: put variables in the "Desolation" struct
    Change-Id: I9ed55cea0c55f0078f8f97ad4037b16ed11c0156

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -1,5 +1,8 @@
-﻿lineage_soong:
+# Insert new variables inside the Desolation structure
+lineage_soong:
 	$(hide) mkdir -p $(dir $@)
 	$(hide) (\
 	echo '{'; \
+	echo '"Desolation": {'; \
+	echo '},'; \
 	echo '') > $(SOONG_VARIABLES_TMP)
